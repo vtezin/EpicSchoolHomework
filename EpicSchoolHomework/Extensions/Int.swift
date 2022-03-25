799c616bb4e097ef3ae73ba69490d6ad96608f2d
Vladimir Tezin <vtezin@gmail.com>
2022-03-25 13:42:26 +0300

    added HeartButton

---
+++ EpicSchoolHomework/Extensions/Int.swift
@@ -0,0 +1,13 @@
+//
+//  Int.swift
+//  EpicSchoolHomework
+//
+//  Created by Vladimir Tezin on 25.03.2022.
+//
+
+import Foundation
+import CoreGraphics
+
+extension Int {
+    var degreesToRadians: CGFloat { return CGFloat(self) * .pi / 180 }
+}
