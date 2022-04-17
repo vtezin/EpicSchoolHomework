d96832ebf2757edc63410a58f6f5c99949dd5127
Vladimir Tezin <vtezin@gmail.com>
2022-04-17 19:12:30 +0300

    tab bar implemented firebase started

--- EpicSchoolHomework/AppDelegate.swift
+++ EpicSchoolHomework/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,8 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         window = .init(frame: UIScreen.main.bounds)
-        window?.rootViewController = UINavigationController(rootViewController: MainScreenViewController())
+        window?.rootViewController = TabBarController()
         window?.makeKeyAndVisible()
+        
+        FirebaseApp.configure()
         
         return true
     }
---
+++ EpicSchoolHomework/Views/TabBarController.swift
@@ -0,0 +1,46 @@
+//
+//  TabBarController.swift
+//  EpicSchoolHomework
+//
+//  Created by Vladimir Tezin on 17.04.2022.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureApperance()
+        
+        viewControllers = [(createNavController(for: MainScreenViewController(), title: NSLocalizedString("Фотки", comment: ""), imageName: "text.below.photo")),
+                           (createNavController(for: LoginViewController(), title: NSLocalizedString("Профиль", comment: ""), imageName: "person"))
+        ]
+        
+    }
+    
+}
+
+// MARK: -  Functions
+extension TabBarController {
+    
+    func configureApperance() {
+        view.backgroundColor = .systemBackground
+        UITabBar.appearance().barTintColor = .systemBackground
+        tabBar.tintColor = .label
+    }
+    
+    func createNavController(for rootViewController: UIViewController,
+                             title: String,
+                             imageName: String) -> UIViewController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        navController.tabBarItem.title = title
+        if let uiImage = UIImage(systemName: imageName) {
+            navController.tabBarItem.image = uiImage
+        }
+        navController.navigationBar.prefersLargeTitles = true
+        rootViewController.navigationItem.title = title
+        return navController
+    }
+    
+}
